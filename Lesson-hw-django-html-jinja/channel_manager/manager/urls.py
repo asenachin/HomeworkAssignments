2afeb11aea61872c7044f19b12628d7da6a1f6df
asenachin <senachin.ap@yandex.ru>
2023-04-20 06:52:58 +0700

    Папка проекта удалена

--- Lesson-hw-django-html-jinja/channel_manager/manager/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('', article, name='home'),
-    path('middle/', article2, name='middle'),
-    path('last/', article3, name='last'),
-]
